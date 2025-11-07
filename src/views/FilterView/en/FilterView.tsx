7abd71b1a8ab3a1cae277ba40b4b956bcee972cc
emre-akakce <emre@akakce.com>
2025-11-07 15:14:38 +0300

    add style for filter

--- src/views/FilterView/en/FilterView.tsx
+++ src/views/FilterView/en/FilterView.tsx
@@ -1,21 +1,21 @@
 import React from "react";
 import type { FilterViewProps } from "../../../models/filter";
+import "../Filter.css";
 
 const FilterView: React.FC<FilterViewProps> = ({ data, viewModel }) => {
   return (
-    <div>
+    <div className="filter-container">
       <h3>Filters</h3>
-      {data?.filters?.map((filter) => (
-        <button
-          key={filter}
-          onClick={() => viewModel.runAttachedFunction("selectFilter", { filter })}
-          style={{
-            fontWeight: data.selectedFilter === filter ? "bold" : "normal",
-          }}
-        >
-          {filter}
-        </button>
-      ))}
+      <div className="filter-buttons">
+        {data?.filters?.map((filter) => (
+          <button
+            key={filter}
+            onClick={() => viewModel.runAttachedFunction("selectFilter", { filter })}
+            className={`filter-button ${data.selectedFilter === filter ? "selected" : ""}`}>
+            {filter}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
