0c4ddcc92bd4cdc5943ad6a662ab172e42422048
emre-akakce <emre@akakce.com>
2025-11-18 16:23:10 +0300

    remove currency sign

--- src/presentation/views/ProductDetailView/ProductDetailView.tsx
+++ src/presentation/views/ProductDetailView/ProductDetailView.tsx
@@ -19,7 +19,7 @@ const ProductDetailView: React.FC<ProductDetailViewProps> = ({ initialData }) =>
     <div className="product-detail-container">
       <h1>{data.product.name}</h1>
       <p><strong>{prTexts.category[data.currentLang]}</strong> {data.product.category}</p>
-      <p><strong>{prTexts.price[data.currentLang]}</strong> ${data.product.price.toFixed(2)}</p>
+      <p><strong>{prTexts.price[data.currentLang]}</strong> {data.product.price.toFixed(2)}</p>
       <p><strong>{prTexts.description[data.currentLang]}</strong> {data.product.description}</p>
     </div>
   );
